f301206fc45cc3ba16a01570247360fa1c714091
Ksenia <shkileva98@mail.ru>
2022-03-01 15:29:56 +0300

    Save arima model for future predictions

--- models/ARIMA/arima_forecast.py
+++ models/ARIMA/arima_forecast.py
@@ -1,9 +1,10 @@
 import pmdarima as pm
 import numpy as np
 import pandas as pd
+import pickle
 
-def read_data(file, col_name='Adj Close'):
-    dataset = pd.read_csv(file, index_col=0, parse_dates=[0])
+def read_data(file, col_name='close'):
+    dataset = pd.read_csv(file, index_col='time', parse_dates=['time'])
     data = dataset.loc[:, col_name]
     data = data.to_frame()
     data = data.sort_index()
@@ -27,7 +28,7 @@ def train_arima(data):
     return model
 
 
-def forecast_arima(file, n_periods, freq='D'):
+def forecast_arima(file, n_periods, freq='D', use_saved_model=True):
     """
     Create a forecast for time series using ARIMA model.
 
@@ -44,6 +45,9 @@ def forecast_arima(file, n_periods, freq='D'):
     'D' - days, 'H' - hours, 'T' - 'minutes'
     Example: n_periods=5, freq='D' -> forecast for 5 days
 
+    use_saved_model: bool, default: True
+    If we use already saved model to predict
+
     Returns
     -------
     List of predicted values
@@ -54,10 +58,17 @@ def forecast_arima(file, n_periods, freq='D'):
         data = data.resample('D').mean()
         data = data.fillna(data.bfill())
 
-    model = train_arima(data)
+    if use_saved_model:
+        with open('auto_arima.pkl', 'rb') as pkl:
+            model = pickle.load(pkl)
+
+        model.update(data)  # update the model with the new data
+    else:
+        model = train_arima(data)
 
     fitted, confint = model.predict(n_periods=n_periods, return_conf_int=True)
     index_of_fc = pd.date_range(start=data.index[-1], periods=n_periods, freq=freq)
 
     fitted_series = pd.DataFrame(fitted, index=index_of_fc, columns=['Value'])
     return list(fitted_series.Value)
+
